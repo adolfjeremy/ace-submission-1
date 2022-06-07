19113743825d6f4a220532239f73318bb95d6a3e
adolfjeremy <adolf.jer@gmail.com>
2022-06-07 23:07:24 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is up and listening at port: ${PORT}`);
+});
